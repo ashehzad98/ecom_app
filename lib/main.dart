797dd99b307e9df16aa441c0f625461b146a3d2c
Ashraf662 <249117420+Ashraf662@users.noreply.github.com>
2026-08-21 17:18:32 +0500

    Implemented Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
+import 'package:ecommerce_app/controllers/auth_controller.dart';
 import 'package:ecommerce_app/controllers/theme_controller.dart';
 import 'package:ecommerce_app/utils/app_theme.dart';
+import 'package:ecommerce_app/view/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_instance/src/extension_instance.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() async {
   await GetStorage.init();
   Get.put(ThemeController());
+  Get.put(AuthController());
   runApp(const MyApp());
 }
 
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
       darkTheme: AppTheme.dark,
       themeMode: themeController.theme,
       defaultTransition: Transition.fade,
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SplashScreen(),
     );
   }
 }
